284ec8ac3c410f32c0345a985d53a9008a749650
sahilkashyap <sahilkashyap64@gmail.com>
2022-07-21 20:25:47 +0530

    targets coustmers and comments added

--- fixDatesWith2DigitYear3.js
+++ fixDatesWith2DigitYear3.js
@@ -15,7 +15,7 @@ MongoClient.connect(DB_URI, options, async function (err, client) {
 
   if (spaced_dates.length!==0) {
   
-    console.log("Messed Up dates convert(b/w year 2000-3000)",spaced_dates);
+    console.log("Messed Up dates convert(targets m/dd/yy , m/d/yy ,mm/d/yy ,mm/dd/yy)",spaced_dates);
     
     const ans = await CMDQuestion.askQuestion("Press y to fix the issues? ");
     console.log("Input :",ans);
@@ -51,14 +51,15 @@ async function findAlldateWithUnusualYearString(client){
 
 
  let objectIdsArr=[ /^\d{2}\/\d{2}\/\d{2}$/, /^\d{2}\/\d{1}\/\d{2}$/,/^\d{1}\/\d{1}\/\d{2}$/,/^\d{1}\/\d{2}\/\d{2}$/ ];
- 
+ // targets m/dd/yy , m/d/yy ,mm/d/yy ,mm/dd/yy
 
  const agg = [
     {
       $match: {
         'dob': {
             $in: objectIdsArr
-        }
+        },
+        '__t': 'Customer' // only  target Customer role
       }
     }, {
       $project: {
@@ -70,9 +71,11 @@ async function findAlldateWithUnusualYearString(client){
           }
         }
       }
+      // all the date to dob string
     }, {
       $count: 'count'
     }
+    //returns count
   ];
   const aggCursor = db.collection(collectionName).aggregate(agg);
   
@@ -100,12 +103,14 @@ async function updateAlldateWithUnusualYearString(client){
   console.log(`Connected to ${dbName} database successfully.`);
  
 let objectIdsArr=[ /^\d{2}\/\d{2}\/\d{2}$/, /^\d{2}\/\d{1}\/\d{2}$/,/^\d{1}\/\d{1}\/\d{2}$/,/^\d{1}\/\d{2}\/\d{2}$/ ];
+// targets m/dd/yy , m/d/yy ,mm/d/yy ,mm/dd/yy
 const targetAll2digityearANdFixUnusalDateConverts=[
     {
       $match: {
         'dob': {
           $in: objectIdsArr
-        }
+        },
+        '__t': 'Customer' // only  target Customer role
       }
     }, {
       $project: {
@@ -117,6 +122,11 @@ const targetAll2digityearANdFixUnusalDateConverts=[
           }
         }
       }
+      /**
+       * _id:62d953e458ef6a20fdf30975
+      dob:"10/07/86"
+     __t:"Customer"
+       */
     }, {
       $match: {
         'result': {
@@ -124,16 +134,24 @@ const targetAll2digityearANdFixUnusalDateConverts=[
           '$lt': new Date('Wed, 31 Dec 3000 00:00:00 GMT')
         }
       }
+      //select the dates between 2000 and 3000, because  mongo can't process year less than 1970
+      /**_id:62d953e458ef6a20fdf30a01
+         result:2062-12-07T00:00:00.000+00:00 */
     }, {
       $project: {
         'dob': {
           '$dateSubtract': {
-            'startDate': '$result', 
+            'startDate': '$result',  //use the result of previous stage
             'unit': 'year', 
             'amount': 100
           }
         }
       }
+      // subtract 100 years to fix the dates
+      /**
+         _id:62d953e458ef6a20fdf30a01
+         dob:1962-12-07T00:00:00.000+00:00
+       */
     }, {
       $addFields: {
         'dob': {
@@ -143,10 +161,15 @@ const targetAll2digityearANdFixUnusalDateConverts=[
           }
         }
       }
+      /** date to string-> 'format': '%Y-%m-%d', 
+       _id:62d953e458ef6a20fdf30a01
+        dob:"1962-12-07"
+       */
     }, {
       $merge: {
         'into': 'dob'
       }
+      /**merge the reult in dob collection */
     }
   ];
 
@@ -176,6 +199,7 @@ const targetAll2digityearANdFixUnusalDateConverts=[
           }
         }
       }
+      //select converts  dates that are not between 2000 and 3000
     }, {
       '$addFields': {
         'dob': {
@@ -192,13 +216,14 @@ const targetAll2digityearANdFixUnusalDateConverts=[
     }
   ];
 
+  //fix dates that are not between 2000 and 3000
 const aggCursor2 = db.collection(collectionName).aggregate(convertallthedates);
 await aggCursor2.forEach(res => {
   console.log("aggCursor2 Unusal date convert fixed",res);
  
   
 });
-
+// fix dates that are  between 2000 and 3000
 const aggCursor = db.collection(collectionName).aggregate(targetAll2digityearANdFixUnusalDateConverts);
 await aggCursor.forEach(res => {
   console.log(" aggCursor Unusal date convert fixed",res);
